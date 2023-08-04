ed1f7efd18dc942ebd902433efa925f2f1966746
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-08-04 15:30:11 +0400

    Receiving New Titles by using UseState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import BookCreate from './components/BookCreate';
 
 export default function App() {
     const [books, setBooks] = useState([]);
 
+    const createBook = (title) => {
+        console.log('Need to add book with: ',title);
+
+    };
+
+
 
     return (
 
-        <div> App </div>
+        <div> 
+            <BookCreate onCreate={createBook} />
+        </div>
     )
 }
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 
-export default function BookCreate() {
+export default function BookCreate({ onCreate }) {
+    const [title, setTitle ] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    };
+
     return (
 
-        <div>BookCreate</div>
+        <div>
+            <form onSubmit={handleSubmit}> 
+                <label>Title</label>
+                <input value={title} onChange={handleChange} />
+                <button>Create!</button>
+            </form>
+        </div>
     )
 }
