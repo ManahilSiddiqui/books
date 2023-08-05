67d44f496d7e432da4c285e88d02f8a43621818a
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-08-06 00:01:30 +0400

    Created a new record to save to API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -28,11 +29,14 @@ export default function App() {
 
     };
 
-    const createBook = (title) => {
-        const updatedBooks = [...books, {
-            id: Math.round(Math.random() * 9999),
-            title,
-        },
+    const createBook = async (title) => {
+       const response = await axios.post('http://localhost:3001/posts', {
+            title
+        });
+
+
+        const updatedBooks = [ ...books,
+            response.data 
     ];
             setBooks(updatedBooks);
 
