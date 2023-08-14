e6a78840e8a4b36d653661ffb997763552f68f0e
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-08-14 15:46:43 +0400

    Created a context object and made use of provider

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 
+
 export default function BookList({ books, onDelete, onEdit }) {
+    const value = useContext(BooksContext);
+
+    
+    
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
@@ -9,6 +16,10 @@ export default function BookList({ books, onDelete, onEdit }) {
 
     return (
 
-        <div className='book-list'> {renderedBooks} </div>
+        <div className='book-list'>
+            {value}
+            {renderedBooks} 
+        
+        </div>
     )
 }
